cd6c3b428351f01e4d66c9989487e85e17a29c14
Manuel Coa <manuelcoa100@gmail.com>
2025-05-22 11:46:54 -0400

    modificacion de nombre de la tabla organizacion_instituciones a organizacion_institucional en consultas sql

--- domains/object/organizacion_instituciones/repositorie.go
+++ domains/object/organizacion_instituciones/repositorie.go
@@ -7,26 +7,26 @@ import (
 )
 
 func selectInstituciones() (*sql.Rows, error) {
-	query := "SELECT id_institucion, id_organizacion, id_jerarquia, id_estructura, nombre_institucion, abreviatura_institucion, dominio_institucion FROM config_organizacion_instituciones"
+	query := "SELECT id_institucion, id_organizacion, id_jerarquia, id_estructura, nombre_institucion, abreviatura_institucion, dominio_institucion FROM config_organizacion_institucional"
 	return repository.FetchRows(query)
 }
 
 func selectInstitucionesID(id int) (*sql.Rows, error) {
-	query := "SELECT id_institucion, id_organizacion, id_jerarquia, id_estructura, nombre_institucion, abreviatura_institucion, dominio_institucion FROM config_organizacion_instituciones WHERE id_institucion = ?"
+	query := "SELECT id_institucion, id_organizacion, id_jerarquia, id_estructura, nombre_institucion, abreviatura_institucion, dominio_institucion FROM config_organizacion_institucional WHERE id_institucion = ?"
 	return repository.FetchRows(query, id)
 }
 
 func insertInstituciones(data Instituciones) (sql.Result, error) {
-	query := "INSERT INTO config_organizacion_instituciones (id_organizacion, id_jerarquia, id_estructura, nombre_institucion, abreviatura_institucion, dominio_institucion) VALUES (?, ?, ?, ?, ?, ?)"
+	query := "INSERT INTO config_organizacion_institucional (id_organizacion, id_jerarquia, id_estructura, nombre_institucion, abreviatura_institucion, dominio_institucion) VALUES (?, ?, ?, ?, ?, ?)"
 	return repository.ExecuteQuery(query, data.OrganizacionID, data.JerarquiaID, data.EstructuraID, data.InstitucionNombre, data.InstitucionAbreviatura, data.InstitucionDominio)
 }
 
 func updateInstituciones(data Instituciones) (sql.Result, error) {
-	query := "UPDATE config_organizacion_instituciones SET id_organizacion = ?, id_jerarquia = ?, id_estructura = ?, nombre_institucion = ?, abreviatura_institucion = ?, dominio_institucion = ? WHERE id_institucion = ?"
+	query := "UPDATE config_organizacion_institucional SET id_organizacion = ?, id_jerarquia = ?, id_estructura = ?, nombre_institucion = ?, abreviatura_institucion = ?, dominio_institucion = ? WHERE id_institucion = ?"
 	return repository.ExecuteQuery(query, data.OrganizacionID, data.JerarquiaID, data.EstructuraID, data.InstitucionNombre, data.InstitucionAbreviatura, data.InstitucionDominio, data.InstitucionID)
 }
 
 func deleteInstituciones(id int) (sql.Result, error) {
-	query := "DELETE FROM config_organizacion_instituciones WHERE id_institucion = ?"
+	query := "DELETE FROM config_organizacion_institucional WHERE id_institucion = ?"
 	return repository.ExecuteQuery(query, id)
 }
